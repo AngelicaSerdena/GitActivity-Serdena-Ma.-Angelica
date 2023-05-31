985bcece6786d8ebd7ddaf545e559ad86bc2f55e
AngelicaSerdena04 <134219954+AngelicaSerdena@users.noreply.github.com>
2023-05-31 05:59:31 -0700

    commit

---
+++ GitActivity-SerdenaMa.Angelica/git-pthon-Serdena.py
@@ -0,0 +1,23 @@
+fname =  input ("What is your full name ?")
+adrs =  input ("What is your address ?")
+bday =  input ("What is your birthday ?")
+print ("")
+print (fname)
+print (adrs)
+print(bday)
+print("")
+
+
+college = input ("What is the name of your school?")
+course = input ("What is your course?")
+year = input ("what year are you?")
+section = input ("What section are you?")
+print ("")
+print (college)
+print (course)
+print(year)
+print("")
+
+
+hobbies = input("what are your 5 hobbies?")
+print(hobbies)
